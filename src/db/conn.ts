98a9929ea525897185dddf6d776a8015a2431f42
Ever <eyuri49@gmail.com>
2025-09-26 16:59:31 -0300

    changed openConnection() to catch async error event

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -1,14 +1,33 @@
 import mariadb from 'mariadb'
 
-export async function openConnection(host: string): Promise<mariadb.PoolConnection> {
-    return await mariadb.createPool({
+export async function openConnection(host: string): Promise<mariadb.PoolConnection | any> {
+
+    const pool = mariadb.createPool({
         host,
         user: 'root',
         password: '240190',
         port: 3306,
         connectionLimit: 5,
-        queryTimeout: 3000,
-        connectTimeout: 3000
+        queryTimeout: 6000,
+        connectTimeout: 6000
     })
-        .getConnection()
+
+    //@ts-ignore
+    pool.on('error', (err) => {
+        console.error('Pool emitted error:', err);
+    });
+
+    const conn = await pool.getConnection()
+
+    const query = async (query: string) => {
+        try {
+            let result = await conn.execute(query)
+            result = Array.isArray(result) ? [...result] : [];
+
+            return result
+        } catch (error) { console.error("Erro na query:", error); return undefined }
+    }
+
+    return { query, release: conn.release }
+
 }
