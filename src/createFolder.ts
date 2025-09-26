f6009855a404d278a4eef9ef0891d50ce1a241a9
Ever <eyuri49@gmail.com>
2025-09-26 16:58:53 -0300

    changed createFolder to create all subFolders too

--- src/createFolder.ts
+++ src/createFolder.ts
@@ -1,12 +1,25 @@
 import fs from "fs";
 import path from "path";
 
-export async function createFolder(machine: string) {
+export async function createFolder(machine: Record<string, any>) {
 
-    const folderPath = path.join(__dirname, '..', 'public', machine);
+    const folderPath = path.join(__dirname, '..', 'public', machine.group);
+    const backupFolderPath = path.join(__dirname, '..', 'public', machine.group, 'backup');
+    const subFolderPath = path.join(__dirname, '..', 'public', machine.group, 'backup', machine.name);
 
     console.log(folderPath);
+    console.log(backupFolderPath);
+    console.log(subFolderPath);
+    
     if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath);
+    }
+
+    if (!fs.existsSync(backupFolderPath)) {
+        fs.mkdirSync(backupFolderPath);
+    }
+
+    if (!fs.existsSync(subFolderPath)) {
+        fs.mkdirSync(subFolderPath);
     }
 }
