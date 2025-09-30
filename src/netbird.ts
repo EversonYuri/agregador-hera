8c488e4a36cf625c90a93a9f60d374a0af5cd0b9
Ever <eyuri49@gmail.com>
2025-09-30 10:08:48 -0300

    changed the grouping to have the computer info

--- src/netbird.ts
+++ src/netbird.ts
@@ -6,25 +6,45 @@ export async function getPeers() {
         headers: { Authorization: 'Bearer nbp_Q3fiAvIBkB4uxFqyfp04rkXDDe8hfz2zhPs4' }
     };
 
+
     let machines = await fetch('https://wweb-api.duckdns.org/api/peers', options)
         .then(response => response.json())
         .catch(err => console.error(err)) as Record<string, any>[]
 
     let editedMachines: any[] = await Promise.all(machines.map(async (machine) => await gatherBasicMachineInfo(machine)));
 
-    const groupSet = new Set<string>();
-    for (const machine of editedMachines) {
-        if (machine && Array.isArray(machine.groups)) {
-            for (const group of machine.groups) {
-                if (group && typeof group.name === 'string') {
-                    groupSet.add(group.name);
-                }
+    const groupSet = new Map<string, any>();
+    for (const machine of editedMachines) if (machine && Array.isArray(machine.groups)) {
+        for (const group of machine.groups) if (group && typeof group.name === 'string') {
+
+            if (groupSet.has(group.name)) {
+                let currentGroup = groupSet.get(group.name);
+                currentGroup.computadores.push({
+                    ip: machine.ip,
+                    name: machine.name,
+                    isServer: machine.isServer,
+                })
+                groupSet.set(currentGroup.name, currentGroup)
+            } else {
+                let currentGroup = { name: group.name }
+                groupSet.set(currentGroup.name, {
+                    ...group, computadores: [
+                        {
+                            ip: machine.ip,
+                            name: machine.name,
+                            isServer: machine.isServer,
+                        }
+                    ]
+                })
             }
+
         }
     }
+    console.log(groupSet);
+
 
     return {
         machines: editedMachines,
-        groups: Array.from(groupSet)
+        groups: groupSet
     }
 }
