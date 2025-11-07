81d53a406513d994505522252406502be09171b9
Ever <eyuri49@gmail.com>
2025-11-07 17:15:15 -0300

    added getGroups route

--- serve.ts
+++ serve.ts
@@ -1,4 +1,5 @@
 import { gatherBasicMachineInfo } from "./src/machine";
+import { createGroups, getComputers } from "./src/netbird";
 
 const server = Bun.serve({
     hostname: "0.0.0.0", 
@@ -9,8 +10,14 @@ const server = Bun.serve({
         switch (url.pathname) {
             case "/peerInfo":
                 const ip = url.searchParams.get("ip");
-                const machineInfo = await gatherBasicMachineInfo({ ip })
+                const name = url.searchParams.get("name");
+                const machineInfo = await gatherBasicMachineInfo({ ip, name })
                 return new Response(JSON.stringify(machineInfo));
+
+            case "/groups": 
+                const computers = await getComputers()       
+                const groupSet = createGroups(computers)
+                return new Response(JSON.stringify(Array.from(groupSet.values())));
             default:
                 return new Response("404 Not Found", { status: 404 });
         }
--- src/netbird.ts
+++ src/netbird.ts
@@ -1,24 +1,38 @@
 import { logMessage } from "./lib/utils";
 import { gatherBasicMachineInfo } from "./machine";
 
-export async function getPeers() {
+export async function getComputers() {
     const options = {
         method: 'GET',
         headers: { Authorization: 'Bearer nbp_PXReq0Q4pPpvIg7yFTVgwUkDLuj6eU2N6T1A' }
     };
 
-
-    let machines = await fetch('https://web-vpn.com.br/api/peers', options)
+    return await fetch('https://web-vpn.com.br/api/peers', options)
         .then(response => response.json())
         .catch(err => console.error(err)) as Record<string, any>[]
+}
+
+export async function getPeers() {
+    let machines = await getComputers()
 
     let editedMachines: any[] = await Promise.all(machines.map(async (machine) => await gatherBasicMachineInfo(machine)));
     logMessage("✅ Terminado de pegar a informação dos peers.");
 
+    const groupSet = createGroups(editedMachines);
+
+    return {
+        machines: editedMachines,
+        groups: groupSet
+    }
+}
+
+export function createGroups(editedMachines: any[]) {
     const groupSet = new Map<string, any>();
     for (const machine of editedMachines) if (machine && Array.isArray(machine.groups)) {
-        for (const group of machine.groups) if (group && typeof group.name === 'string') {
+        for (let group of machine.groups) if (group && typeof group.name === 'string') {
 
+            group.name = group.name.toUpperCase();
+            
             if (groupSet.has(group.name)) {
                 let currentGroup = groupSet.get(group.name);
                 currentGroup.computadores.push({
@@ -42,8 +56,5 @@ export async function getPeers() {
         }
     }
 
-    return {
-        machines: editedMachines,
-        groups: groupSet
-    }
+    return groupSet;
 }
