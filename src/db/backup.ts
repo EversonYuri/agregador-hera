7b4f934421906131bb5dc02af63b01044764a9f6
Ever <eyuri49@gmail.com>
2025-11-14 17:12:47 -0300

    testing backup

--- src/db/backup.ts
+++ src/db/backup.ts
@@ -6,89 +6,102 @@ export async function backupDatabase(host: string, dbName: string, saveLocation:
   const date = now.toISOString().split("T")[0];
   const outputFile = path.join(saveLocation, `${dbName}_${date}.sql.gz`);
 
-  logMessage(`Backing up ${dbName} to ${outputFile}`);
-  let dump
+  // logMessage(`Backing up ${dbName} to ${outputFile}`);
+  console.log("docker", "exec", "-i", "mariadb",  
+    "mariadb-dump",
+    "-h", host,
+    "-u", Bun.env.USUARIO_DB || '',
+    `-p${Bun.env.SENHA_DB}`,
+    "--skip-ssl",
+    "--single-transaction",
+    "--quick",
+    "--compress",
+    "--routines",
+    "--events",
+    "--triggers",
+    dbName)
 
-  if (process.platform === "win32") {
-    dump = Bun.spawn([
-      "C:/HERA/BANCO/bin/mysqldump",
-      "-h", host,
-      "-u", Bun.env.USUARIO_DB || '',
-      `-p${Bun.env.SENHA_DB}`,
-      "--single-transaction",
-      "--quick",
-      "--compress",
-      "--routines",
-      "--events",
-      "--triggers",
-      dbName
-    ], {
-      stdio: ["ignore", "pipe", "inherit"]
-    });
-  } else {
-    dump = Bun.spawn([
-      "docker", "exec", "-i", "mariadb",  // nome do container
-      "mysqldump",
-      "-u", Bun.env.USUARIO_DB || '',
-      `-p${Bun.env.SENHA_DB}`,
-      "--single-transaction",
-      "--quick",
-      "--compress",
-      "--routines",
-      "--events",
-      "--triggers",
-      dbName
-    ], {
-      stdio: ["ignore", "pipe", "inherit"]
-    });
-  }
-
-  let gzip
-
-  // run gzip
-  if (process.platform === "win32") {
-    gzip = Bun.spawn([
-      "C:/Program Files (x86)/GnuWin32/bin/gzip.exe",
-      "-c"
-    ], {
-      stdin: "pipe",
-      stdout: "pipe",
-      stderr: "inherit"
-    });
-  } else {
-    gzip = Bun.spawn(["gzip", "-c"], {
-      stdin: "pipe",
-      stdout: "pipe",
-      stderr: "inherit",
-    });
-  }
-
-  // connect mysqldump → gzip
-  // dump.stdout.pipeTo(gzip.stdin);
-
-  // manually pipe:
-  {
-    const reader = dump.stdout.getReader();
-    const writer = gzip.stdin as any; // FileSink
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) break;
-      writer.write(value);
-    }
-    writer.end();
-  }
+  // // run mariadb-dump
+  // let dump
+  // if (process.platform === "win32") {
+  //   dump = Bun.spawn([
+  //     "C:/HERA/BANCO/bin/mysqldump",
+  //     "-h", host,
+  //     "-u", Bun.env.USUARIO_DB || '',
+  //     `-p${Bun.env.SENHA_DB}`,
+  //     "--single-transaction",
+  //     "--quick",
+  //     "--compress",
+  //     "--routines",
+  //     "--events",
+  //     "--triggers",
+  //     dbName
+  //   ], {
+  //     stdio: ["ignore", "pipe", "inherit"]
+  //   });
+  // } else {
+  //   dump = Bun.spawn([
+  //     "docker", "exec", "-i", "mariadb",  // nome do container
+  //     "mariadb-dump",
+  //     "-h", host,
+  //     "-u", Bun.env.USUARIO_DB || '',
+  //     `-p${Bun.env.SENHA_DB}`,
+  //     "--skip-ssl",
+  //     "--single-transaction",
+  //     "--quick",
+  //     "--compress",
+  //     "--routines",
+  //     "--events",
+  //     "--triggers",
+  //     dbName
+  //   ], {
+  //     stdio: ["ignore", "pipe", "inherit"]
+  //   });
+  // }
 
 
-  const outFile = Bun.file(outputFile);
-  // await gzip.stdout.pipeTo(outFile.writable); /
+  // // run gzip
+  // let gzip
+  // if (process.platform === "win32") {
+  //   gzip = Bun.spawn([
+  //     "C:/Program Files (x86)/GnuWin32/bin/gzip.exe",
+  //     "-c"
+  //   ], {
+  //     stdin: "pipe",
+  //     stdout: "pipe",
+  //     stderr: "inherit"
+  //   });
+  // } else {
+  //   gzip = Bun.spawn(["gzip", "-c"], {
+  //     stdin: "pipe",
+  //     stdout: "pipe",
+  //     stderr: "inherit",
+  //   });
+  // }
 
-  const writer = outFile.writer();
+  // // manually pipe:
+  // {
+  //   const reader = dump.stdout.getReader();
+  //   const writer = gzip.stdin as any; // FileSink
+  //   while (true) {
+  //     const { value, done } = await reader.read();
+  //     if (done) break;
+  //     writer.write(value);
+  //   }
+  //   writer.end();
+  // }
 
-  // Pipe gzip.stdout into a manual loop:
-  for await (const chunk of gzip.stdout) {
-    writer.write(chunk);
-  }
-  await writer.end();
 
-  logMessage(`terminado o backup ${dbName}`)
+  // const outFile = Bun.file(outputFile);
+  // // await gzip.stdout.pipeTo(outFile.writable); /
+
+  // const writer = outFile.writer();
+
+  // // Pipe gzip.stdout into a manual loop:
+  // for await (const chunk of gzip.stdout) {
+  //   writer.write(chunk);
+  // }
+  // await writer.end();
+
+  // logMessage(`terminado o backup ${dbName}`)
 }
